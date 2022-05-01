fdbf069b919def100bdab905380d6cff189d7ab3
shouskee <rummy5163@gmail.com>
2022-05-01 15:05:16 +0900

    正規表現で、バリデーション訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
         
   with_options presence: true do
    validates :nickname, presence: true
-   validates :second_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥ー]+\z/ }
-   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥ー]+\z/ }
+   validates :second_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
    validates :second_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
    validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
    validates :birthday, presence: true
