f9fca7ff7253db49161c45bbd467d9fd309df3f4
shouskee <rummy5163@gmail.com>
2022-05-01 15:07:35 +0900

    ルーティング訂正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   resources :items do
-    resources :purchases, only: [:index, :new, :create]
+    resources :purchases, only: [:index, :create]
   end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
