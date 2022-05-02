7b00bfb1c4fa093de628dadf9a88a743571deeb2
shouskee <rummy5163@gmail.com>
2022-05-02 19:57:30 +0900

    フリマアプリ挙動訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,6 @@ class Item < ApplicationRecord
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :product_name, presence: true
   validates :explanation, presence: true
-  validates :selling_price, presence: true
   validates :image, presence: true
 
   validates :selling_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
