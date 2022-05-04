713027d3c8b79799d44166b64e072599353947e2
shouskee <rummy5163@gmail.com>
2022-05-04 17:59:31 +0900

    商品購入モデル単体テスト日本語エラー文

--- spec/models/purchase_information_spec.rb
+++ spec/models/purchase_information_spec.rb
@@ -21,62 +21,62 @@ RSpec.describe PurchaseInformation, type: :model do
     it 'tokenが空では登録できないこと' do
       @purchase_information.token = ''
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Token can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("カード情報を入力してください")
     end
     it 'user_idが空では登録できない' do
       @purchase_information.user_id = ''
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("User can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("Userを入力してください")
     end
     it 'item_idが空では登録できない' do
       @purchase_information.item_id = ''
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Item can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("Itemを入力してください")
     end
     it 'postal_codeが空では登録できない' do
       @purchase_information.postal_code = ''
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Postal code can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("郵便番号を入力してください")
     end
     it 'postal_codeは「3桁ハイフン4桁」の半角文字列のみ保存可能なこと' do
       @purchase_information.postal_code = '1234567'
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      expect(@purchase_information.errors.full_messages).to include('郵便番号に(-)を入れて下さい')
     end
     it 'area_idが空では登録できない' do
       @purchase_information.area_id = 1
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Area can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("都道府県を入力して下さい")
     end
     it 'city_nameが空では登録できない' do
       @purchase_information.city_name = ''
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("City name can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("市区町村を入力してください")
     end
     it 'addressが空では登録できない' do
       @purchase_information.address = ''
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Address can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("番地を入力してください")
     end
     it 'telephone_numberが空では登録できない' do
       @purchase_information.telephone_number = ''
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Telephone number can't be blank")
+      expect(@purchase_information.errors.full_messages).to include("電話番号を入力してください")
     end
     it 'telephone_numberは10桁以上11桁以内の半角数値のみ保存可能なこと' do
       @purchase_information.telephone_number = '090-1234-5678'
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Telephone number is invalid. Include hyphen(-)")
+      expect(@purchase_information.errors.full_messages).to include("電話番号に(-)を入れずに入力して下さい")
     end
     it 'telephone_numberは9桁以下では購入できない' do
       @purchase_information.telephone_number = '09012345'
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Telephone number is invalid. Include hyphen(-)")
+      expect(@purchase_information.errors.full_messages).to include("電話番号に(-)を入れずに入力して下さい")
     end
     it 'telephone_numberは12桁以上では購入できない' do
       @purchase_information.telephone_number = '090123456789'
       @purchase_information.valid?
-      expect(@purchase_information.errors.full_messages).to include("Telephone number is invalid. Include hyphen(-)")
+      expect(@purchase_information.errors.full_messages).to include("電話番号に(-)を入れずに入力して下さい")
     end
   end
 end
